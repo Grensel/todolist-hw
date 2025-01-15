044ea869d921724a1003dfc6ae8db58ff421ae5a
Grensel <sergeiodinochenko@mail.ru>
2025-01-15 12:44:34 +0300

    feat: completed second tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import { S } from "./hw/components/TodolistItem_Styles";
+import { HomeWork1 } from "./hw/HW1/HomeWork01-1";
 import { HomeWork2 } from "./hw/HW2/HomeWork01-2";
 
 function App() {
   return (
     <S.TodolistHW>
-      {/* <HomeWork1 /> */}
+      <HomeWork1 />
       <HomeWork2 />
       {/* <HomeWork3 /> */}
     </S.TodolistHW>
